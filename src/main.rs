21c86441894b38a6049ced78728b306e17f4ac63
Nadji Abidi <mouhamir2@gmail.com>
2024-06-22 15:12:57 +0100

    Apply changes to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-pub mod action;
 pub mod app;
 pub mod draw;
 pub mod render;
 pub mod state;
 
 use app::App;
-use render::RenderState;
-use state::{AppState, WindowState};
-
 use winit::event_loop::{ControlFlow, EventLoop};
 
 fn main() {
     // create and run the App
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Wait);
-    let mut app = App {
-        app_state: AppState::new(),
-        render_state: RenderState::default(),
-        window_state: WindowState::new(),
-    };
+    let mut app = App::default();
     event_loop.run_app(&mut app).unwrap();
 }
