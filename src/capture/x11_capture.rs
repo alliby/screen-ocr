414cbbba6d04305934d8cbf6dda7c99054520c07
alliby <mouhamir2@gmail.com>
2024-09-08 04:47:13 +0100

    Fix Linux compilation errors

--- src/capture/x11_capture.rs
+++ src/capture/x11_capture.rs
@@ -1,14 +1,12 @@
 use anyhow::Result;
-use std::error::Error;
-use std::fs::File;
-use std::io::Write;
+use std::sync::Arc;
 use vello::kurbo::Rect;
 use vello::peniko::Blob;
 use x11rb::connection::Connection;
 use x11rb::protocol::xproto;
-use xproto::ImageFormat::Z_PIXMAP;
+use xproto::ImageFormat;
 
-fn screen_rect(rect: Rect) -> Result<Blob<u8>> {
+pub fn screen_rect(rect: Rect) -> Result<Blob<u8>> {
     let (conn, screen_num) = x11rb::connect(None)?;
     let screen = &conn.setup().roots[screen_num];
     let root = screen.root;
@@ -18,14 +16,8 @@ fn screen_rect(rect: Rect) -> Result<Blob<u8>> {
         rect.width().abs() as u16,
         rect.height().abs() as u16,
     );
-    let reply = xproto::get_image(&conn, Z_PIXMAP, root, x, y, w, h, u32::MAX)?.reply()?;
+    let reply = xproto::get_image(&conn, ImageFormat::Z_PIXMAP, root, x, y, w, h, u32::MAX)?.reply()?;
 
-    let data = reply.data;
-    let mut bytes = vec![(rect.width * rect.heigth) as usize * 4].into_boxed_slice();
-    for chunck in data.chunks(4) {
-        let pixel = [chunk[2], chunk[1], chunk[0], 255];
-        bytes.write(&pixel[..])?;
-    }
-
-    Ok(Arc::from(bytes.into()))
+    let data = reply.data.into_boxed_slice();
+    Ok(Blob::new(Arc::new(data)))
 }
